e24d30ab4e7eac9696fc0ec31248da83be6fbec2
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2022-01-04 11:23:53 +0900

    Add block or non-block checking section

--- with-errgroup/with-errgoup.go
+++ with-errgroup/with-errgoup.go
@@ -19,27 +19,40 @@ func main() {
 	g, gCtx := errgroup.WithContext(ctx)
 
 	g.Go(func() error {
+
 		for {
+			// NOTE - Default Selection
+			// The default case in a select is run if no other case is ready.
+			// Use a default case to try a send or receive without blocking:
+
 			select {
 			case <-gCtx.Done():
 				fmt.Println("Break the loop")
 				return nil
 			case <-time.After(1 * time.Second):
 				fmt.Println("Hello in a loop")
+				// default:
+				// 	fmt.Print(".")
 			}
+
+			fmt.Println("Bottom of for loop")
+			// time.Sleep(100 * time.Millisecond)
 		}
 	})
 
 	g.Go(func() error {
-		for {
-			select {
-			case <-gCtx.Done():
-				fmt.Println("Break the loop")
-				return nil
-			case <-time.After(1 * time.Second):
-				fmt.Println("Ciao in a loop")
-			}
-		}
+		<-gCtx.Done()
+		fmt.Println("Caio in Second routine")
+		return nil
+		// for {
+		// 	select {
+		// 	case <-gCtx.Done():
+		// 		fmt.Println("Break the loop")
+		// 		return nil
+		// 	case <-time.After(1 * time.Second):
+		// 		fmt.Println("Ciao in a loop")
+		// 	}
+		// }
 	})
 
 	err := g.Wait()
